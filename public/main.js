920878df857e3a66c8d70af5965f72bf6ed26a40
kaussandeep <kaus.sandeep@gmail.com>
2020-05-31 01:14:03 +0200

    added error handling

--- public/main.js
+++ public/main.js
@@ -1,49 +1,47 @@
-const $input = $('#search');
+
 const $submit = $('#button');
 
-  $("#button").click(function() {
-    var countryIsoCode = $('#search').val();
-    const urlToFetch = 'https://api.covid19api.com/country/'+countryIsoCode;
-    $.ajax({
-      type: 'GET',
-      async: false,
-      url: urlToFetch,
-      dataType: 'json',
-      success: function(response) {
-        var searchCountry = response[0].Country;
-        var maxConfirmedCount = response[0].Confirmed;
-        var maxConfirmedDate = response[0].Date;
-        var maxDeathCount = response[0].Deaths;
-        var maxDeathDate = response[0].Date;
-         for (i=1; i< response.length; i++) {
-            var todaysConfirmedCount = response[i].Confirmed - response[i-1].Confirmed; 
-            if (todaysConfirmedCount > maxConfirmedCount ) {
-              maxConfirmedCount = todaysConfirmedCount;
-              maxConfirmedDate = response[i].Date;
+$("#button").click(function () {
+  var countryIsoCode = $('#search').val();
+  const urlToFetch = 'https://api.covid19api.com/country/' + countryIsoCode;
+  $.ajax({
+    type: 'GET',
+    async: false,
+    url: urlToFetch,
+    dataType: 'json',
+    success: function (response) {
+      var searchCountry = response[0].Country;
+      var maxConfirmedCount = response[0].Confirmed;
+      var maxConfirmedDate = response[0].Date;
+      var maxDeathCount = response[0].Deaths;
+      var maxDeathDate = response[0].Date;
+      for (i = 1; i < response.length; i++) {
+        var todaysConfirmedCount = response[i].Confirmed - response[i - 1].Confirmed;
+        if (todaysConfirmedCount > maxConfirmedCount) {
+          maxConfirmedCount = todaysConfirmedCount;
+          maxConfirmedDate = response[i].Date;
 
-            }
-            var todaysDeathCount = response[i].Deaths - response[i-1].Deaths; 
-            if (todaysDeathCount > maxDeathCount ) {
-              maxDeathCount = todaysDeathCount;
-              maxDeathDate = response[i].Date;
-
-            }
         }
+        var todaysDeathCount = response[i].Deaths - response[i - 1].Deaths;
+        if (todaysDeathCount > maxDeathCount) {
+          maxDeathCount = todaysDeathCount;
+          maxDeathDate = response[i].Date;
 
-  //    $("#confirmedCases").update(confirmedCases);
-    //  $("#confirmedDeaths").update(confirmedCases);
-    
-      document.getElementById('responseField').innerHTML = 'Country :'  + searchCountry + ' Max Confirmed Count for single day:'  +maxConfirmedCount +  'Date:'   + maxConfirmedDate ;
+        }
+      }
 
-      alert('Country: ' + searchCountry +' \nMax Confirmed Count for single day : ' +maxConfirmedCount + '\n Date : ' + maxConfirmedDate);
-      //alert('Max Death Count in a single Day : ' + maxDeathCount + '\n Day : ' + maxDeathDate);
-      return maxConfirmedCount ;
-    }, 
-    error: function(XMLHttpRequest, textStatus, errorThrown) { 
-      alert("Incorrect ISO code entered: " + textStatus); alert("Error is :  " + errorThrown); 
-  }    
-  
-    });
-    
-    
+      //    $("#confirmedCases").update(confirmedCases);
+      //  $("#confirmedDeaths").update(confirmedCases);
+
+      document.getElementById('responseField').innerHTML = 'Country :' + searchCountry + ' Max Confirmed Count for single day:' + maxConfirmedCount + 'Date:' + maxConfirmedDate;
+      return maxConfirmedCount;
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      alert("Incorrect ISO code entered: " + textStatus);
+      alert("Error is :  " + errorThrown);
+    }
+
+  });
+
+
 });
--- public/main2.js
+++ public/main2.js
@@ -1,48 +1,60 @@
-const $input = $('#search');
 const $submit = $('#button');
 
 const getCoronaResults = (callback) => {
-    var countryIsoCode = $('#search').val();
-const urlToFetch = 'https://api.covid19api.com/country/'+countryIsoCode;
-//const wordQuery = $input.value;
+  var countryIsoCode = $('#search').val();
+  const urlToFetch = 'https://api.covid19api.com/country/' + countryIsoCode;
+  //const wordQuery = $input.value;
 
-const xhr = new XMLHttpRequest();
-xhr.responseType = 'json';
-xhr.onreadystatechange = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status == 404) {
+        alert("You entered an incorrect ISO Code : " + countryIsoCode);
+        //throw new Error(urlToFetch + ' replied 404');
+      } else {
         var searchCountry = xhr.response[0].Country;
         var maxConfirmedCount = xhr.response[0].Confirmed;
         var maxConfirmedDate = xhr.response[0].Date;
         var maxDeathCount = xhr.response[0].Deaths;
         var maxDeathDate = xhr.response[0].Date;
-         for (i=1; i< xhr.response.length; i++) {
-            var todaysConfirmedCount = xhr.response[i].Confirmed - xhr.response[i-1].Confirmed; 
-            if (todaysConfirmedCount > maxConfirmedCount ) {
-              maxConfirmedCount = todaysConfirmedCount;
-              maxConfirmedDate = xhr.response[i].Date;
+        for (i = 1; i < xhr.response.length; i++) {
+          var todaysConfirmedCount = xhr.response[i].Confirmed - xhr.response[i - 1].Confirmed;
+          if (todaysConfirmedCount > maxConfirmedCount) {
+            maxConfirmedCount = todaysConfirmedCount;
+            maxConfirmedDate = xhr.response[i].Date;
 
-            }
-            var todaysDeathCount = xhr.response[i].Deaths - xhr.response[i-1].Deaths; 
-            if (todaysDeathCount > maxDeathCount ) {
-              maxDeathCount = todaysDeathCount;
-              maxDeathDate = xhr.response[i].Date;
+          }
+          var todaysDeathCount = xhr.response[i].Deaths - xhr.response[i - 1].Deaths;
+          if (todaysDeathCount > maxDeathCount) {
+            maxDeathCount = todaysDeathCount;
+            maxDeathDate = xhr.response[i].Date;
 
-            }
+          }
         }
 
-      document.getElementById('responseField').innerHTML = 'Country :'  + searchCountry + ' Max Confirmed Count for single day:'  +maxConfirmedCount +  'Date:'   + maxConfirmedDate +
-         'Max Death Count in a single Day : ' + maxDeathCount + '\n Day : ' + maxDeathDate;
-      //alert('Country: ' + searchCountry +' \nMax Confirmed Count for single day : ' +maxConfirmedCount + '\n Date : ' + maxConfirmedDate);
+        document.getElementById('responseField').innerHTML = 'Country :' + searchCountry + ' Max Confirmed Count for single day:' + maxConfirmedCount + 'Date:' + maxConfirmedDate +
+          'Max Death Count in a single Day : ' + maxDeathCount + '\n Day : ' + maxDeathDate;
+        //alert('Country: ' + searchCountry +' \nMax Confirmed Count for single day : ' +maxConfirmedCount + '\n Date : ' + maxConfirmedDate);
+
+      }
+
+    }
   }
-  }
-xhr.open('GET', urlToFetch);
-xhr.send();
+  xhr.open('GET', urlToFetch);
+  xhr.send();
+}
+
+
+const exit = async () => {
+  console.log("invalid");
+  document.getElementById('responseField').innerHTML = "";
 }
 
 // Clear previous results and display results to webpage
 const displaySuggestions = (event) => {
   event.preventDefault()
-  while(responseField.firstChild){
+  while (responseField.firstChild) {
     responseField.removeChild(responseField.firstChild)
   };
   getCoronaResults();
